f93886bf174355aad8049b49ec2593a829f08531
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-09-06 19:50:22 -0300

    First haskell proyect

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import MiModulo
+import Data.List.Split
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Por favor, ingresa una lista de valores separados por comas:"
+    input <- getLine
+    if input == "salir"
+        then putStrLn "Hasta la vista, Baby! 😎"
+        else do
+            let valores = splitOn "," input
+            let resultado = if allEqual valores then "Son todos iguales" else "No son todos iguales"
+            putStrLn ("El resultado es: " ++ resultado)
+            main
---
+++ Haskell/app/MiModulo.hs
@@ -0,0 +1,6 @@
+module MiModulo where
+
+allEqual :: Eq a => [a] -> Bool
+allEqual [] = True
+allEqual [_] = True
+allEqual (x:y:xs) = (x == y) && allEqual (y:xs)
