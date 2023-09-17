5be3379f4a1aa149af3ca322efd6444151118ff2
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-09-17 17:50:36 -0300

    Added Haskell SchemyInterpreter

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -22,9 +22,29 @@ main = do
     --        ("value", JSONString "admin")
     --        ]
     --putStrLn (show crearObjeto)
-    
-        let uno = SchemyNumber 1
-        let dos = SchemyNumber 2
-        let exp = SchemyAdd uno dos
-        let resultado = eval (fromList []) exp
-        putStrLn $ "El resultado de la suma es: " ++ show resultado
+    let env = fromList [("pi", SchemyNumber (3.141592653589793)), ("e", SchemyNumber (exp 1.0))]
+    let e1 = SchemyMult (SchemySymbol "pi") (SchemySymbol "pi")
+    let e2 = SchemySymbol "e"
+    let e3 = SchemyAdd (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e4 = SchemyEqual (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e5 = SchemyLessThanOrEqual (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e6 = SchemyNot (SchemyValue (SchemyBool True))
+    let e7 = SchemyAnd (SchemyValue (SchemyBool True)) (SchemyValue (SchemyBool False))
+    let e8 = SchemyGreaterThan (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e9 = SchemyGreaterThanOrEqual (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e10 = SchemyLessThan (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e11 = SchemyNotEqual (SchemyValue (SchemyNumber 5)) (SchemyValue (SchemyNumber 3))
+    let e12 = SchemyOr (SchemyValue (SchemyBool True)) (SchemyValue (SchemyBool False))
+
+    putStrLn $ "Resultado de e1: " ++ show (eval env e1)
+    putStrLn $ "Resultado de e2: " ++ show (eval env e2)
+    putStrLn $ "Resultado de e3: " ++ show (eval env e3)
+    putStrLn $ "Resultado de e4: " ++ show (eval env e4)
+    putStrLn $ "Resultado de e5: " ++ show (eval env e5)
+    putStrLn $ "Resultado de e6: " ++ show (eval env e6)
+    putStrLn $ "Resultado de e7: " ++ show (eval env e7)
+    putStrLn $ "Resultado de e8: " ++ show (eval env e8)
+    putStrLn $ "Resultado de e9: " ++ show (eval env e9)
+    putStrLn $ "Resultado de e10: " ++ show (eval env e10)
+    putStrLn $ "Resultado de e11: " ++ show (eval env e11)
+    putStrLn $ "Resultado de e12: " ++ show (eval env e12)
--- Haskell/app/SchemyInterpreter.hs
+++ Haskell/app/SchemyInterpreter.hs
@@ -1,25 +1,78 @@
 module SchemyInterpreter where
     
+import Data.Map (Map)
 import qualified Data.Map as Map
 
+type SchemyEnv = Map String SchemyValue
+
 data SchemyExp
-    = SchemyNumber Double
+    = SchemyValue SchemyValue
     | SchemyAdd SchemyExp SchemyExp
     | SchemyMult SchemyExp SchemyExp
     | SchemySymbol String
+    | SchemyEqual SchemyExp SchemyExp
+    | SchemyLessThanOrEqual SchemyExp SchemyExp
+    | SchemyNot SchemyExp
+    | SchemyAnd SchemyExp SchemyExp
+    | SchemyOr SchemyExp SchemyExp
+    | SchemyGreaterThan SchemyExp SchemyExp
+    | SchemyGreaterThanOrEqual SchemyExp SchemyExp
+    | SchemyLessThan SchemyExp SchemyExp
+    | SchemyNotEqual SchemyExp SchemyExp
     deriving (Eq)
 
-eval :: (Map String Double) -> SchemyExp -> Double
-eval _ (SchemyNumber n) = n
-eval env (SchemyAdd a b) = eval env a + eval env b
-eval env (SchemyMult a b) = eval env a * eval env b
+data SchemyValue
+    = SchemyNumber Double
+    | SchemyBool Bool
+    deriving (Eq)
+
+eval :: SchemyEnv -> SchemyExp -> SchemyValue
+eval env (SchemyValue v) = v
+eval env (SchemyAdd a b) = SchemyNumber (left (eval env a) + left (eval env b))
+eval env (SchemyMult a b) = SchemyNumber (left (eval env a) * left (eval env b))
+eval env (SchemyEqual a b) = SchemyBool (eval env a == eval env b)
+eval env (SchemyLessThanOrEqual a b) = SchemyBool (left (eval env a) <= left (eval env b))
+eval env (SchemyAnd a b) = case (eval env a, eval env b) of
+    (SchemyBool x, SchemyBool y) -> SchemyBool (x && y)
+    _ -> error "Operadores lógicos solo pueden ser aplicados a valores booleanos o numéricos"
+eval env (SchemyOr a b) = case (eval env a, eval env b) of
+    (SchemyBool x, SchemyBool y) -> SchemyBool (x || y)
+    _ -> error "Operadores lógicos solo pueden ser aplicados a valores booleanos o numéricos"
+eval env (SchemyNot a) = case eval env a of
+    SchemyBool x -> SchemyBool (not x)
+    _ -> error "Operador lógico 'not' solo puede ser aplicado a valores booleanos o numéricos"
+eval env (SchemyGreaterThan a b) = SchemyBool (left (eval env a) > left (eval env b))
+eval env (SchemyGreaterThanOrEqual a b) = SchemyBool (left (eval env a) >= left (eval env b))
+eval env (SchemyNotEqual a b) = SchemyBool (eval env a /= eval env b)
+eval env (SchemyLessThan a b) = SchemyBool (left (eval env a) < left (eval env b))
 eval env (SchemySymbol s) = case Map.lookup s env of
     Just value -> value
     Nothing    -> error ("Símbolo no definido: " ++ s)
 
+
+left :: SchemyValue -> Double
+left (SchemyNumber d) = d
+left _ = error "Se esperaba un SchemyNumber"
+
+right :: SchemyValue -> Bool
+right (SchemyBool b) = b
+right _ = error "Se esperaba un SchemyBool"
+
 instance Show SchemyExp where
-    show (SchemyNumber n) = show n 
-    show (SchemySymbol s) = show s 
+    show (SchemySymbol s) = s
     show (SchemyAdd a b) = "(" ++ show a ++ ")" ++ " + " ++ "(" ++ show b ++ ")" 
     show (SchemyMult c d) = "(" ++ show c ++  ")" ++ " * " ++ "(" ++ show d ++ ")" 
-    show (SchemyMult c d) = "(" ++ show c ++  ")" ++ " * " ++ "(" ++ show d ++ ")" 
+    show (SchemyEqual a b) = "(" ++ show a ++  ")" ++ " == " ++ "(" ++ show b ++ ")"
+    show (SchemyLessThanOrEqual a b) = "(" ++ show a ++  ")" ++ " <= " ++ "(" ++ show b ++ ")"
+    show (SchemyNot a) = "not (" ++ show a ++ ")"
+    show (SchemyAnd a b) = "(" ++ show a ++  ") && (" ++ show b ++ ")"
+    show (SchemyGreaterThan a b) = "(" ++ show a ++  ")" ++ " > " ++ "(" ++ show b ++ ")"
+    show (SchemyGreaterThanOrEqual a b) = "(" ++ show a ++  ")" ++ " >= " ++ "(" ++ show b ++ ")"
+    show (SchemyLessThan a b) = "(" ++ show a ++  ")" ++ " < " ++ "(" ++ show b ++ ")"
+    show (SchemyNotEqual a b) = "(" ++ show a ++  ")" ++ " != " ++ "(" ++ show b ++ ")"
+    show (SchemyOr a b) = "(" ++ show a ++  ") || (" ++ show b ++ ")"
+
+    
+instance Show SchemyValue where
+    show (SchemyNumber n) = show n
+    show (SchemyBool b) = show b
